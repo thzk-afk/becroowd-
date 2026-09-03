74b668e4687ab755e9e962a2df6292f73dc466eb
thzk-afk <thalisson.rsilva0@gmail.com>
2026-09-03 17:25:35 -0300

    1003

---
+++ 1003.cs
@@ -0,0 +1,12 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+ int n1 = int.Parse(Console.ReadLine());
+ int n2 = int.Parse(Console.ReadLine());
+ Console.WriteLine($"soma={n1 + n2}");
+
+    }
+
+}
