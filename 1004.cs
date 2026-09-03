740783da06a702ad434d8059417572bea8877c46
thzk-afk <thalisson.rsilva0@gmail.com>
2026-09-03 17:26:15 -0300

    1004

---
+++ 1004.cs
@@ -0,0 +1,16 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+     int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+
+int PROD = n1 * n2;
+
+Console.WriteLine($"PROD = {PROD}");
+
+    }
+
+}
