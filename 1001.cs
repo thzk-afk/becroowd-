978994c129287f3615032c9d83acf57325910a13
thzk-afk <thalisson.rsilva0@gmail.com>
2026-09-03 17:24:08 -0300

    1001

---
+++ 1001.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+    
+     int n = int.Parse(Console.ReadLine());
+
+     
+     int n2 = int.Parse(Console.ReadLine());
+
+     Console.WriteLine($"X = {n + n2}");
+
+    }
+
+}
